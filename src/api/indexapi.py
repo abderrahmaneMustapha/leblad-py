49b98a5ec4bd03a9520a87393e9d1dd8cb5774ed
abderrahmane <abderrahmaneMustapha030898@gmail.com>
2020-11-23 22:21:11 +0100

    feat :  add get wilaya list , get wilaya object by wilaya code

---
+++ src/api/indexapi.py
@@ -0,0 +1,18 @@
+import json
+
+class Api:
+    data = json.load(open("../../data/WilayaList.json", encoding="utf-8"))
+    
+    def getWilayaList(self):
+        wilaya_list = [ d['name'] for d in self.data ]
+        return  wilaya_list
+    def getWilayaByCode(self,  wilaya_code):
+        def search(_data, wilaya_code):
+            return list(filter(lambda i :  i['mattricule'] == wilaya_code, _data))
+        
+        return search(self.data, wilaya_code)[0]
+    
+
+
+a  =  Api()
+
