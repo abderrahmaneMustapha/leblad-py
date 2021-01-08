5c9d04772a6b7574e9bda3b7875239c551dbeb87
abderrahmane <abderrahmaneMustapha030898@gmail.com>
2021-01-08 18:40:48 +0100

    add a docstring  for leblad __init__.py

--- leblad/__init__.py
+++ leblad/__init__.py
@@ -1,1 +1,6 @@
+"""
+this file is required to make Python treat this directories as 
+containing package; this is done to prevent directories with a common name, such as string,
+ from unintentionally hiding valid modules that occur later on the module search path.
+"""
 from leblad.indexapi import Api
