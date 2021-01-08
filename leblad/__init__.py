37b8a1a4c421fec8b30acec92d5f066c2320d180
abderrahmane <abderrahmaneMustapha030898@gmail.com>
2021-01-08 18:56:36 +0100

    fix : leblad init file linting

--- leblad/__init__.py
+++ leblad/__init__.py
@@ -1,5 +1,5 @@
 """
-this file is required to make Python treat this directories as 
+this file is required to make Python treat this directories as
 containing package; this is done to prevent directories with a common name, such as string,
  from unintentionally hiding valid modules that occur later on the module search path.
 """
